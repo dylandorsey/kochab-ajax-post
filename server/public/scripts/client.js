e366ab1b59727aadff6078218c662929f2f8744e
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-19 11:28:58 -0500

    serves /records and appends records-module data to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,27 @@ $(document).ready(onReady);
 
 function onReady () {
     console.log('in jQ');
+    appendAllRecords();
+}
+
+function appendAllRecords() {
+    $.ajax({
+        type: 'GET',
+        url: '/records'
+    })
+    .then( function (response){
+        for (let i = 0; i < response.length; i++) {
+            let formattedCost = formatNumberAsCurrency(response[i].cost);
+            $('#recordList').append(`<li>${response[i].title}</li>`);
+            $('#recordList').append(`<p>Year: ${response[i].year}</p>`);
+            $('#recordList').append(`<p>Artist: ${response[i].artist}</p>`);
+            $('#recordList').append(`<p>Cost: $${formattedCost}</p>`);
+        }
+        console.log(response);
+
+    });
+}
+
+function formatNumberAsCurrency (number) {
+    return number.toLocaleString('USD');
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = 5000;
 
+// this type of line (data) will live in the database soon
+const recordCollection = require('./modules/record-collection');
+
 app.use(express.static('server/public'));
+
+// routes (get, post, put, and delete requests)
+app.get('/records', (req, res) => {
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`...and we're live on port: ${PORT}`);
 });
+
+
