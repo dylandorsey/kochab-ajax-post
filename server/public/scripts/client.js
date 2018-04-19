b69ae39424b4d51fd1a9ae9fed2f0c8405f0854f
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-19 11:59:48 -0500

    appends data to DOM as table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,16 +9,18 @@ function onReady () {
 
 function appendAllRecords() {
     $.ajax({
-        type: 'GET',
+        method: 'GET',
         url: '/records'
     })
     .then( function (response){
         for (let i = 0; i < response.length; i++) {
             let formattedCost = formatNumberAsCurrency(response[i].cost);
-            $('#recordList').append(`<li>${response[i].title}</li>`);
-            $('#recordList').append(`<p>Year: ${response[i].year}</p>`);
-            $('#recordList').append(`<p>Artist: ${response[i].artist}</p>`);
-            $('#recordList').append(`<p>Cost: $${formattedCost}</p>`);
+            $('#recordList').append(`<tr>`)
+            $('#recordList').append(`<td>${response[i].title}</li>`);
+            $('#recordList').append(`<td>${response[i].year}</p>`);
+            $('#recordList').append(`<td>${response[i].artist}</p>`);
+            $('#recordList').append(`<td>${formattedCost}</p>`);
+            $('#recordList').append(`</td>`)
         }
         console.log(response);
 
@@ -26,5 +28,5 @@ function appendAllRecords() {
 }
 
 function formatNumberAsCurrency (number) {
-    return number.toLocaleString('USD');
+    return number.toLocaleString('en', { style: 'currency', currency: 'USD'}).slice(0,-3);
 }
